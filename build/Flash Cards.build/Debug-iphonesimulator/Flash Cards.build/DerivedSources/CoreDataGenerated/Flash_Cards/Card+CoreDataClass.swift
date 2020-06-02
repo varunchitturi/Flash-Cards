d2d8455b7e02a25dd5296849e6d4458a255b9d1e
Varun Chitturi <varunchitturi@icloud.com>
2020-06-02 12:51:05 -0700

    build

---
+++ build/Flash Cards.build/Debug-iphonesimulator/Flash Cards.build/DerivedSources/CoreDataGenerated/Flash_Cards/Card+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Card+CoreDataClass.swift
+//  
+//
+//  Created by Varun Chitturi on 6/2/20.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Card)
+public class Card: NSManagedObject {
+
+}
